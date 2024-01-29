e83d8b756d1c06c0ba3eaa18bb029d599e9c5776
MahfuzzamanSizan <sizanmahfuzzaman@gmail.com>
2024-01-29 14:35:59 +0600

    third commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,26 +1,44 @@
 import "./list.css"
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import { useLocation } from "react-router-dom"
+import { useState } from "react"
+import { format } from "date-fns"
 
 
 const List = () => {
 
+  const locaion = useLocation()
+
+  const [destination,setDestination] = useState(locaion.state.destination)
+  const [date,setDate] = useState(locaion.state.date)
+  const [options,setOptions] = useState(locaion.state.options)
+
 
   return (
     <div>
-      <Navbar/> 
-      <Header type="list"/>
+      <Navbar />
+      <Header type="list" />
 
       <div className="listContainer">
         <div className="listWrapper">
-            <div className="listSearch">
+          <div className="listSearch">
             <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input placeholder={destination} type="text" />
             </div>
-                
-            <div className="listResult">
 
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
             </div>
-            
+          </div>
+
+          <div className="listResult">
+
+          </div>
+
         </div>
       </div>
 
